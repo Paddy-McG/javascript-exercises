6c94d770abbe495a2a082bf2d2d40669477fbf96
Patrick McGonagle <120601884+Paddy-McG@users.noreply.github.com>
2023-02-03 15:07:45 +0000

    Finishing sum all

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    if(typeof a === "number" && typeof b === "number" && a > 0 && b > 0) {
+        var big =(a > b)? a : b;
+        var small = (a < b) ? a : b;
+        var total = 0;
+        while(small <= big) {
+            total += small;
+            small++;
+        }
+        return total;
+    } else {
+        return "ERROR";
+    }
 };
 
 // Do not edit below this line
