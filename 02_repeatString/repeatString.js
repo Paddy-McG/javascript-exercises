d78755643b4e4ac976c8b0c46d4bb46c18838ac7
Patrick McGonagle <120601884+Paddy-McG@users.noreply.github.com>
2023-02-03 12:22:39 +0000

    Repeat string Finished

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    var i =0;
+    var s1 = "";
+    if(num < 0) {
+        s1 = "ERROR";
+    } else {
+        while (i < num) {
+            s1 += str;
+            i++;
+        }
+    }
+    return s1;
 };
 
 // Do not edit below this line
