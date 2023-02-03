960539224ff7964ac2757d40db3d1c7b48474ff1
Patrick McGonagle <120601884+Paddy-McG@users.noreply.github.com>
2023-02-03 14:35:32 +0000

    Fixing remove From Array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(a, b, c, d, e) {
+    var ray = a;
+    var i = 0;
+    while(i < ray.length) {
+        if(ray[i] === b | ray[i] === c | ray[i] === d | ray[i] === e) {
+            ray.splice(i, 1);
+        } else {
+            ++i;
+        }
+    }
+    return ray;
 };
 
 // Do not edit below this line
