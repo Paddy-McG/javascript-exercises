43b7e94c0db351f8d200556808358c794bc0e90f
Patrick McGonagle <120601884+Paddy-McG@users.noreply.github.com>
2023-02-03 13:11:43 +0000

    Finishing reverse String Task

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    var len = str.length;
+    const ray = str.split("");
+    let s1 = "";
+    for(var i = 0; i < len; i++) {
+        s1 = ray[i] + s1;
+    }
+    return s1;
 };
 
 // Do not edit below this line
