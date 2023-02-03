86edbda156e70aac1da131216b1669781a28d523
Patrick McGonagle <120601884+Paddy-McG@users.noreply.github.com>
2023-02-03 19:20:38 +0000

    Finishing Temperature Conversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,20 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(num) {
+  var cel = ((num - 32) * 5/9);
+  if(cel % 1 === 0) {
+    return cel
+  } else {
+    return parseFloat(cel.toFixed(1));
+  }
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(num) {
+  var fern = ((num * 9/5) + 32);
+  if (fern % 1 === 0) {
+    return fern;
+  } else {
+    return parseFloat(fern.toFixed(1));
+  }
 };
 
 // Do not edit below this line
